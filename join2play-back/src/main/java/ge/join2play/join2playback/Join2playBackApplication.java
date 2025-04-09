1206835f2631a975a9ff4bc7290fcb22cdb276f7
Tsiqara <mtsik21@freeuni.edu.ge>
2025-04-09 13:24:54 +0400

    Add backend module to project

---
+++ join2play-back/src/main/java/ge/join2play/join2playback/Join2playBackApplication.java
@@ -0,0 +1,13 @@
+package ge.join2play.join2playback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Join2playBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Join2playBackApplication.class, args);
+    }
+
+}
---
+++ join2play-back/src/test/java/ge/join2play/join2playback/Join2playBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.join2play.join2playback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Join2playBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
