177175dc89e94bad05005e97f1bdbf377f429709
Nika Kekelishvili <113893418+nkeke21@users.noreply.github.com>
2025-05-28 21:06:44 +0400

    Make sign in component to be in separate file

---
+++ finalproject_hub/src/components/auth/SignInForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <n-card title="Sign In" class="auth-card">
+        <n-form>
+            <n-form-item label="Username">
+                <n-input v-model:value="username" placeholder="Enter username" />
+            </n-form-item>
+
+            <n-form-item label="Password">
+                <n-input type="password" v-model:value="password" placeholder="Enter password" />
+            </n-form-item>
+
+            <n-button type="primary" @click="signIn" color="orange">Sign In</n-button>
+        </n-form>
+
+        <div class="signup-switch">
+            Don't have an account?
+            <n-button text color="orange" @click="$emit('switchToSignUp')">Sign Up</n-button>
+        </div>
+    </n-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { NInput, NButton, NForm, NFormItem, NCard } from 'naive-ui'
+
+const username = ref('')
+const password = ref('')
+
+const signIn = () => {
+    console.log('Sign-in with', username.value, password.value)
+}
+</script>
