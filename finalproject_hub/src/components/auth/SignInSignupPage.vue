3b291eeb997cf3b96b9acd71ac93ba2130fc8f11
Nika Kekelishvili <113893418+nkeke21@users.noreply.github.com>
2025-05-28 20:55:23 +0400

    Add left and right panel for better user experience

--- finalproject_hub/src/components/auth/SignInSignupPage.vue
+++ finalproject_hub/src/components/auth/SignInSignupPage.vue
@@ -1,10 +1,44 @@
 <template>
+    <div class="auth-container">
+        <div class="left-panel">
+            <img src="@/assets/Join2Play.png" alt="Join2Play Logo" class="auth-icon" />
+        </div>
 
+        <div class="right-panel">
+        
+        </div>
+    </div>
 </template>
-
-<script lang="ts">
-
+  
+<script setup>
 </script>
-
+  
 <style scoped>
+.auth-container {
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+}
+
+.left-panel {
+    background-color: var(--dark);
+    width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.auth-icon {
+    width: 160px;
+    height: auto;
+}
+
+.right-panel {
+    width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+}
 </style>
+  
