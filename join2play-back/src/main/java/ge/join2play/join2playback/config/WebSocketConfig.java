72faac1c300cf3acf9be12af1eb2f4c4b64227c8
Nika Kekelishvili <113893418+nkeke21@users.noreply.github.com>
2025-05-29 23:26:09 +0400

    Added dto

--- join2play-back/src/main/java/ge/join2play/join2playback/config/WebSocketConfig.java
+++ join2play-back/src/main/java/ge/join2play/join2playback/config/WebSocketConfig.java
@@ -1,4 +1,21 @@
 package ge.join2play.join2playback.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-public class WebSocketConfig {
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
 }
--- join2play-back/src/main/java/ge/join2play/join2playback/model/dto/NotificationDTO.java
+++ join2play-back/src/main/java/ge/join2play/join2playback/model/dto/NotificationDTO.java
@@ -1,4 +1,15 @@
 package ge.join2play.join2playback.model.dto;
 
 public class NotificationDTO {
+    private String message;
+    private String eventId;
+    private String senderUserId;
+
+    public NotificationDTO() {}
+
+    public NotificationDTO(String message, String eventId, String senderUserId) {
+        this.message = message;
+        this.eventId = eventId;
+        this.senderUserId = senderUserId;
+    }
 }
