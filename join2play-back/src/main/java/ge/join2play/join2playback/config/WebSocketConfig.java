691ddef9f3534c6d31c47e25bc7a8e87d1c9bc5a
Nika Kekelishvili <113893418+nkeke21@users.noreply.github.com>
2025-05-29 23:42:17 +0400

    Fixing issues related to websockets

--- join2play-back/src/main/java/ge/join2play/join2playback/config/WebSocketConfig.java
+++ join2play-back/src/main/java/ge/join2play/join2playback/config/WebSocketConfig.java
@@ -1,11 +1,17 @@
 package ge.join2play.join2playback.config;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -17,5 +23,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        messageConverters.add(new MappingJackson2MessageConverter());
+        return false;
     }
 }
--- join2play-back/src/main/java/ge/join2play/join2playback/model/dto/NotificationDTO.java
+++ join2play-back/src/main/java/ge/join2play/join2playback/model/dto/NotificationDTO.java
@@ -1,8 +1,16 @@
 package ge.join2play.join2playback.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class NotificationDTO {
+
+    @JsonProperty("message")
     private String message;
+
+    @JsonProperty("eventId")
     private String eventId;
+
+    @JsonProperty("senderUserId")
     private String senderUserId;
 
     public NotificationDTO() {}
@@ -10,6 +18,30 @@ public class NotificationDTO {
     public NotificationDTO(String message, String eventId, String senderUserId) {
         this.message = message;
         this.eventId = eventId;
+        this.senderUserId = senderUserId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public String getSenderUserId() {
+        return senderUserId;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setSenderUserId(String senderUserId) {
         this.senderUserId = senderUserId;
     }
 }
