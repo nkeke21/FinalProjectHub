e7d8341d926b170f2484f2ad4619c17d18cff3ab
Nika Kekelishvili <113893418+nkeke21@users.noreply.github.com>
2025-07-15 21:42:26 +0400

    Update SportEvent model object

--- finalproject_hub/src/models/SportEvent.ts
+++ finalproject_hub/src/models/SportEvent.ts
@@ -1,12 +1,16 @@
 export interface SportEvent {
     eventId: string | null
-    hostName: string |null
+    hostId: string | null
+    hostName: string | null
     latitude: number | null
     longitude: number | null
     location: string
     participants: number
+    numberOfParticipantsTotal: number
+    numberOfParticipantsRegistered: number
     ageRange: string
     eventTime: number | null
     sportType: string | null
     description: string | null
+    participantsList?: Array<{ name: string; age: number }>
 }
