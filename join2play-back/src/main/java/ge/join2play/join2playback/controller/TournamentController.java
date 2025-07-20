4031005d17220f58cd6005cd8f716b04abb79be7
Nika Kekelishvili <113893418+nkeke21@users.noreply.github.com>
2025-07-20 23:36:15 +0400

    returned http session library in tournamentscontroller

--- join2play-back/src/main/java/ge/join2play/join2playback/controller/TournamentController.java
+++ join2play-back/src/main/java/ge/join2play/join2playback/controller/TournamentController.java
@@ -7,6 +7,7 @@ import ge.join2play.join2playback.service.TournamentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
 
 import java.util.List;
 import java.util.UUID;
